944ac64e8a996db0df4e6271727abacb8f190412
Nicomoguel <nicolasmm0711@gmail.com>
2026-06-18 16:18:11 -0600

    18/6/26.1

--- graficador/MiPanel.java
+++ graficador/MiPanel.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import javax.swing.BorderFactory;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -19,62 +20,80 @@ public class MiPanel extends JPanel{
 
     public MiPanel(){
         setBorder(BorderFactory.createLineBorder(Color.black));
-        int[] coeficientes = {0,0,1};
+        int[] coeficientes = {1,0,1};
         this.p = new Polinomio(coeficientes);
         this.xInicial = -5;
         this.xFinal = 5;
         this.yInicial = -5;
         this.yFinal = 5;
-        addMouseListener(new MouseAdapter(){
-            public void mousePressed(MouseEvent e){
-                moveSquare(e.getX(), e.getY());
-            }
-        });
     }
 
     private void graficarFuncion(Graphics g){
-        double x, y, sigX, sigY;
-        double siguienteX, siguienteY;
-        for(int pixel = 0; pixel < this.getWidth(); pixel++){
-            x = escalar(pixel);
+        int pixelY, sigX, sigY;
+        double x, y, siguienteX, siguienteY;
+        for(int pixelX = 0; pixelX < this.getWidth(); pixelX++){
+            x = escalarX(pixelX);
             y = this.p.evaluar(x);
+            pixelY = escalarY(y);
+            sigX = pixelX + 1;
+            siguienteX = escalarX(sigX);
+            siguienteY = this.p.evaluar(siguienteX);
+            sigY = escalarY(siguienteY);
+            g.drawLine(pixelX, pixelY, sigX, sigY);
+
         }
         
     }
+
+    private void dibujarPlano(Graphics g){
+        g.drawLine(this.getWidth()/2, 0, this.getWidth()/2, this.getHeight());
+        g.drawLine(0, this.getHeight()/2, this.getWidth(), this.getHeight()/2);
+        int numeroX = -5;
+        int numeroY = 5;
+        for(int pixel = 0; pixel < this.getWidth(); pixel++){
+            if(pixel % (this.getWidth() / 10) == 0){
+                g.drawLine(pixel, (this.getHeight()/2)-10, pixel, (this.getHeight()/2)+10); 
+            }
+            if(pixel % (this.getHeight() / 10) == 0){ 
+                g.drawLine((this.getWidth()/2)+10, pixel+4, (this.getWidth()/2)-10, pixel+4); 
+            }
+            if(pixel  % (this.getWidth() / 10) == 0){
+                if(numeroX != 0){
+                    g.drawString(String.valueOf(numeroX), pixel, (this.getHeight() / 2) - 12);   
+                }
+                numeroX++;
+            }
+            if(pixel % (this.getHeight() / 10) == 0){
+                g.drawString(String.valueOf(numeroY), this.getHeight() / 2, pixel);
+                numeroY--;
+            }
+        }
+    }
+
+        
+    private int escalarY(double y){
+        double yEscalada = -y;
+        // invertimos la grafica por que las coordenadas del panel son de (0,0) en la esquina superior izquierda
+        yEscalada = yEscalada * this.getHeight() / (this.yFinal - this.yInicial); 
+        return (int)yEscalada + (this.getHeight() / 2);
+    }
+
     
     private double escalarX(int x){
         double xEscalada = x - this.getWidth() / 2.0;
         xEscalada /= this.getWidth();
         xEscalada *= (this.xFinal - this.xInicial);
-        return xEscalada
+        return xEscalada;
     }
 
-    private void moveSquare(int x, int y){
-        int OFFSET = 1;
-        if(squareX != x || squareY != y){
-            repaint(squareX, squareY, squareW+OFFSET, squareH+OFFSET);
-            squareX = x;
-            squareY = y;
-            repaint(squareX, squareY, squareW+OFFSET, squareH+OFFSET);
-        }
-    }   
 
     public Dimension getPreferredSize(){
-        return new Dimension(250, 200);
+        return new Dimension(250, 250);
     }
     protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        g.drawString("Este es un ejemplo", 10, 20);
-        g.setColor(Color.RED);
-        g.fillRect(squareX,squareY,squareW,squareH);
-        g.setColor(Color.BLACK);
-        g.drawRect(squareX,squareY,squareW,squareH);
-        // dibujamos los ejes coordenados
-        g.drawLine(this.getWidth()/2, 0, this.getWidth()/2, this.getHeight());
-        g.drawLine(0, this.getHeight()/2, this.getWidth(), this.getHeight()/2);
-        g.drawLine(0, this.getHeight(), this.getWidth(), 0);
-        g.drawLine(0, 0, this.getWidth(), this.getHeight());
-
+        g.setFont(new Font("SANS_SERIF", Font.PLAIN, 10));
+        g.drawString("f(x) = x^2 + 1", 40, 50);
+        this.dibujarPlano(g);
         graficarFuncion(g);
     }
 }
